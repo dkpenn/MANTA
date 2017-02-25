e82de4211f2c82384f3292b1996d4f7f2701dfde
dkpenn <dhriti@seas.upenn.edu>
2017-02-25 14:14:02 -0500

    commented out slides

--- app/src/test/java/com/mymanet/manta/PacketTest.java
+++ app/src/test/java/com/mymanet/manta/PacketTest.java
@@ -37,42 +37,42 @@ public class PacketTest {
 
     }
 
-    @Test
-    public void packetPath() throws Exception {
-        String path = "Hello";
-        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK, path, 0);
-        assertNotNull(p.path);
-        assertEquals(p.path.size(), 1);
-        assertEquals(p.path.get(0), "Hello");
-    }
-
-    @Test
-    public void packetPathTwo() throws Exception {
-        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK);
-        p.addToPath("Hello");
-        String pathString = p.pathToString();
-        Packet p2 = new Packet("hello.txt", 10, "Lord", PacketType.ACK, pathString, 0);
-        assertNotNull(p2.path);
-        assertEquals(p2.path.size(), 1);
-        assertEquals("Hello", p2.path.get(0));
-    }
-
-    @Test
-    public void packetPathThree() throws Exception {
-        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK);
-        p.addToPath("Hello");
-        p.addToPath("Goodbye");
-        p.addToPath("Where");
-        p.addToPath("Hero");
-        String pathString = p.pathToString();
-        Packet p2 = new Packet("hello.txt", 10, "Lord", PacketType.ACK, pathString, 0);
-        assertNotNull(p2.path);
-        assertEquals(p2.path.size(), 4);
-        assertEquals("Hello", p2.path.get(0));
-        assertEquals("Goodbye", p2.path.get(1));
-        assertEquals("Where", p2.path.get(2));
-        assertEquals("Hero" , p2.path.get(3));
-    }
+//    @Test
+//    public void packetPath() throws Exception {
+//        String path = "Hello";
+//        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK, path, 0);
+//        assertNotNull(p.);
+//        assertEquals(p.path.size(), 1);
+//        assertEquals(p.path.get(0), "Hello");
+//    }
+//
+//    @Test
+//    public void packetPathTwo() throws Exception {
+//        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK);
+//        p.addToPath("Hello");
+//        String pathString = p.pathToString();
+//        Packet p2 = new Packet("hello.txt", 10, "Lord", PacketType.ACK, pathString, 0);
+//        assertNotNull(p2.path);
+//        assertEquals(p2.path.size(), 1);
+//        assertEquals("Hello", p2.path.get(0));
+//    }
+//
+//    @Test
+//    public void packetPathThree() throws Exception {
+//        Packet p =  new Packet("hello.txt", 10, "Lord", PacketType.ACK);
+//        p.addToPath("Hello
+//        p.addToPath("Goodbye");
+//        p.addToPath("Where");
+//        p.addToPath("Hero");
+//        String pathString = p.pathToString();
+//        Packet p2 = new Packet("hello.txt", 10, "Lord", PacketType.ACK, pathString, 0);
+//        assertNotNull(p2.path);
+//        assertEquals(p2.path.size(), 4);
+//        assertEquals("Hello", p2.path.get(0));
+//        assertEquals("Goodbye", p2.path.get(1));
+//        assertEquals("Where", p2.path.get(2));
+//        assertEquals("Hero" , p2.path.get(3));
+//    }
 
     @Test
     public void pathStringOne() throws Exception {
